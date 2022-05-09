abaca62ec96fa730b0ccd312a4a027bcd10c3665
joshuaabolade09 <65114596+joshuaabolade09@users.noreply.github.com>
2022-05-09 15:11:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let btnprimt=document.querySelector('#primt ')
+
+let btnprimrt=document.querySelector('#primirit ')
+btnprimt.addEventListener('click' ,()=>btnprimrt.style.backgroundColor='green' )
